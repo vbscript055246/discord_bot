2c808a2a06c40e20fe06d81a50ce2c014615b92e
vbscript055246 <vbscript055246@gmail.com>
2018-04-10 12:28:42 +0800

    make it better

--- VBS_discord_bot.py
+++ VBS_discord_bot.py
@@ -10,7 +10,7 @@ if not discord.opus.is_loaded():
     # you should replace this with the location the
     # opus library is located in and with the proper filename.
     # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+    discord.opus.load_opus('libopus-0.x64.dll')
 
 # Client = discord.Client()
 prefix = "!"
