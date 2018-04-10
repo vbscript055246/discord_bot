a92f2b03cb640063afa1278173e54c769ceb35b2
vbscript055246 <vbscript055246@gmail.com>
2018-04-10 13:34:21 +0800

    make it better

--- VBS_discord_bot.py
+++ VBS_discord_bot.py
@@ -3,7 +3,20 @@ from discord.ext import commands
 from bs4 import BeautifulSoup
 import requests
 import re
-from discord.opus import OpusNotLoaded
+from discord import opus
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+if opus.is_loaded():
+    return True
+
+for opus_lib in opus_libs:
+    try:
+        opus.load_opus(opus_lib)
+        return
+    except OSError:
+        pass
 
 # Client = discord.Client()
 prefix = "!"
